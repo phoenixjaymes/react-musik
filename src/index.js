0c26f544d5c3ad21ce12dac0fa108bc1e394617e
Jaymes Young <jaymes@phoenixjaymes.com>
2019-10-10 14:32:09 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {
+  BrowserRouter,
+} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import { Provider } from './components/Context';
+
+ReactDOM.render(
+  // <BrowserRouter basename="/lab/r-bs-music/">
+  <BrowserRouter>
+    <Provider>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('app'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
