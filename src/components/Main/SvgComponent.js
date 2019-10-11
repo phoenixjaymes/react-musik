f99b15f20df07aa3e1f52f9850c10fe7af14ef01
Jaymes Young <jaymes@phoenixjaymes.com>
2019-10-11 15:10:03 -0700

    added gradient and various styles

--- src/components/Main/SvgComponent.js
+++ src/components/Main/SvgComponent.js
@@ -2,10 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SvgComponent = ({ title }) => (
-  <svg viewBox="0 0 400 110" xmlns="http://www.w3.org/2000/svg">
-    <rect width="100%" height="100%" style={{ fill: 'greenyellow', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
-    <text x="50%" y="50%" textAnchor="middle" dominantBaseline="middle" fill="red" fontSize="2rem">{title}</text>
-  </svg>
+  <div className="svg-banner">
+    <svg viewBox="0 0 700 120" xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <radialGradient id="gradient">
+          <stop offset="0%" stopColor="#000" />
+          <stop offset="50%" stopColor="#000" />
+          <stop offset="100%" stopColor="#b00" />
+        </radialGradient>
+      </defs>
+
+      <rect width="100%" height="100%" fill="url(#gradient)" style={{ strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+      <text x="50%" y="55%" textAnchor="middle" dominantBaseline="middle" fill="#fff" fontSize="3rem" fontFamily="Arial, Helvetica, sans-serif">{title}</text>
+    </svg>
+  </div>
 );
 
 SvgComponent.propTypes = {
