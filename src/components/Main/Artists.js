3a44554a0174121ff4cb30c9b3fca9490f3864e1
Jaymes Young <jaymes@phoenixjaymes.com>
2019-10-11 14:49:59 -0700

    removed all test code

--- src/components/Main/Artists.js
+++ src/components/Main/Artists.js
@@ -3,23 +3,14 @@ import PropTypes from 'prop-types';
 import { Container } from 'react-bootstrap';
 
 import GroupContainer from './GroupContainer';
-import ArtistAlbumsTest from './ArtistsAlbumsTest';
 
 const Artists = ({ context }) => {
-  const { artistGroups, showDetails, showDetailsId } = context;
+  const { artistGroups } = context;
 
   return (
     <Container as="section">
       <h1 className="text-center">Musiker</h1>
       <GroupContainer groups={artistGroups} groupType="artist" />
-
-      { showDetails === true && (
-        <div className="music-details">
-          <span onClick={context.actions.showDetailsModal}>+++</span>
-          <ArtistAlbumsTest type="all" id={showDetailsId} />
-        </div>
-      )}
-
     </Container>
   );
 };
