63de8a1c644a16e7cb2343d90cb255af34f2eb22
Jaymes Young <jaymes@phoenixjaymes.com>
2019-10-12 16:23:35 -0700

    imported withContext and added setGroupToOpen func to nav onCLick

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,32 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import {
   Container,
   Navbar,
 } from 'react-bootstrap';
 
+import withContext from '../Context';
+
 import '../../css/header.css';
 
-const Header = () => (
-  <Navbar as="header" className="main-header p-1" fixed="top" bg="red" expand="xs">
-    <Container className="p-0">
+const Header = ({ context }) => {
+  const { actions } = context;
+  const { setGroupToOpen } = actions;
 
-      <nav>
-        <ul className="utilities">
-          <li><NavLink to="/" exact>Musik</NavLink></li>
-          <li>&nbsp;</li>
-          <li><span>Filtern</span></li>
-        </ul>
-        <ul className="menu">
-          <li><NavLink to="/favs-lists" exact>Listen</NavLink></li>
-          <li><NavLink to="/artists">Musiker</NavLink></li>
-          <li><NavLink to="/albums">Alben</NavLink></li>
-          <li><NavLink to="/genres">Genres</NavLink></li>
-          <li><NavLink to="/songs">Lieder</NavLink></li>
-        </ul>
-      </nav>
-    </Container>
-  </Navbar>
-);
+  return (
+    <Navbar as="header" className="main-header p-1" fixed="top" bg="red" expand="xs">
+      <Container className="p-0">
 
-export default Header;
+        <nav onClick={() => setGroupToOpen('')} role="presentation">
+          <ul className="utilities">
+            <li><NavLink to="/" exact>Musik</NavLink></li>
+            <li>&nbsp;</li>
+            <li><span>Filtern</span></li>
+          </ul>
+          <ul className="menu">
+            <li><NavLink to="/favs-lists" exact>Listen</NavLink></li>
+            <li><NavLink to="/artists">Musiker</NavLink></li>
+            <li><NavLink to="/albums">Alben</NavLink></li>
+            <li><NavLink to="/genres">Genres</NavLink></li>
+            <li><NavLink to="/songs">Lieder</NavLink></li>
+          </ul>
+        </nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+Header.propTypes = {
+  context: PropTypes.shape(),
+};
+
+export default withContext(Header);
